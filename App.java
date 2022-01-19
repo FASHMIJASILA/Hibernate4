556582f7975cd9217f3672546a170261e08ad951
B K Jasila Fashmi <57026764+FASHMIJASILA@users.noreply.github.com>
2022-01-19 20:38:07 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.cg.hibernatee.SecndHqlQuery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,67 @@
+package com.cg.hibernatee.SecndHqlQuery;
+
+public class Employee {
+	 private int id;
+	   private String firstName; 
+	   private String lastName;   
+	   private int salary;  
+
+	   
+	   public Employee( String firstName, String lastName, int salary) {
+		super();
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+
+	public Employee() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	
+	   }
+	   
+	
